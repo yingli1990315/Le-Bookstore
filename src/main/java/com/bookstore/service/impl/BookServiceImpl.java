56734a5f2db302565ee9d424c3ee3dc6e97c7840
yingli1990315 <yingli1990315@gmail.com>
2017-05-01 17:25:03 -0700

    second commit

---
+++ src/main/java/com/bookstore/service/impl/BookServiceImpl.java
@@ -0,0 +1,69 @@
+package com.bookstore.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.Book;
+import com.bookstore.repository.BookRepository;
+import com.bookstore.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Override
+	public List<Book> findAll() {
+		List<Book> bookList =  (List<Book>)bookRepository.findAll();
+		List<Book> activeBookList = new ArrayList<>();
+		
+		for(Book book: bookList){
+			if(book.isActive()){
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
+	}
+
+	@Override
+	public Book findOne(Long id) {
+		return bookRepository.findOne(id);
+	}
+
+	@Override
+	public List<Book> findByCategory(String category) {
+		List<Book> bookList = bookRepository.findByCategory(category);
+		
+		List<Book> activeBookList = new ArrayList<>();
+		
+		for(Book book: bookList){
+			if(book.isActive()){
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
+	}
+
+	@Override
+	public List<Book> blurrySearch(String keyword) {
+		List<Book> bookList = bookRepository.findByTitleContaining(keyword);
+		
+		List<Book> activeBookList = new ArrayList<>();
+		
+		for(Book book: bookList){
+			if(book.isActive()){
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
+		
+	}
+	
+}
